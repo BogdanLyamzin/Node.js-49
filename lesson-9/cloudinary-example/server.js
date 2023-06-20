cb1cce58478124d87c31b445df7a341565c388fd
unknown <bogdan.lyamzin.d@gmail.com>
2023-06-20 20:56:43 +0300

    finish lesson 9

---
+++ lesson-9/cloudinary-example/server.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const app = require('./app');
+
+const {DB_HOST} = process.env;
+
+mongoose.connect(DB_HOST)
+  .then(()=> app.listen(3000))
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
---
+++ lesson-9/project-backend/middlewares/index.js
@@ -0,0 +1,11 @@
+const isBodyEmpty = require("./isBodyEmpty");
+const isValidId = require("./isValidId");
+const authenticate = require("./authenticate");
+const upload = require("./upload");
+
+module.exports = {
+    isBodyEmpty,
+    isValidId,
+    authenticate,
+    upload,
+}
